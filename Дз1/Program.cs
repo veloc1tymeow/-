b662c71c10d985ab176c6f435d5c2bec45bd4f1e
veloc1ty <odexta02@gmail.com>
2023-10-27 19:50:28 +0300

    Добавил решение задачи 1

---
+++ Дз1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Counter(int num, int i = 1)
+{
+    if(num >= i)
+    {
+        Console.Write(num + " ");
+        num = num - 1;
+        Counter(num, 1);
+    }
+    else
+    {
+        return;
+    }
+}
+Counter(n, 1);
