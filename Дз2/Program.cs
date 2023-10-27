391415df651588db53933b5fd91bd08462368d21
veloc1ty <odexta02@gmail.com>
2023-10-27 19:57:45 +0300

    Выполнил задание 2

--- Дз2/Program.cs
+++ Дз2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for(int i =n1; i <= n2; i++)
+{
+    count = count + i;
+}
+Console.WriteLine(count);
